2775fa2727a9fb948a65da7c52161dbe2f7191cc
Thiago <thiago.caitanov@gmail.com>
2023-06-20 17:05:15 -0300

    apresentation finalization

---
+++ utils.lua
@@ -0,0 +1,20 @@
+local utils = {}
+
+---
+---Start the terminal using UTF-8
+---
+os.execute("chcp 65001")
+
+local function apresentation()
+    print("===========================================\nBATTLE FOR HEREDIN\n===============================================")
+    print("Heredin a grande capital da Terra-Mediana®, gorvernada por um tirano malévolo que utilizava\n do poder de sua pulsereira para subtrair seus inimigos e inquisitores a 0!")
+    print("A Terra-Mediana® precisa de um bravo herói que utilize dos poderes do protagonismos para livra-los deste mal!")
+    print("================================================================================================================")
+    print()
+
+    
+
+end
+
+
+return utils
