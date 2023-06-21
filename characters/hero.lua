77740ef6d26a3257d80108ecf6b093a9ef17f400
Thiago Caitano <thiago.caitanov@gmail.com>
2023-06-21 15:10:04 -0300

    interface finalization

--- characters/hero.lua
+++
@@ -1,9 +0,0 @@
-local hero = {}
-
-hero.name = "Violet"
-hero.maxHealth = 10
-hero.health = 10
-hero.attack = 4
-hero.defense = 2
-
-return hero
--- main.lua
+++
@@ -1,8 +0,0 @@
-local utils = require("config/utils")
-local interface = require("events/interface")
-
-print([[
-
-pode colocar uma espadona aqui
-
-]])
